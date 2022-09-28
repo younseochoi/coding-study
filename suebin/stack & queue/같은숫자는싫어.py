366124853c63ae1cd5eb8df886c06adacacfebd8
suebin <suuebin@gmail.com>
2022-09-28 23:07:41 +0900

    [스택/큐] 같은 숫자는 싫어

--- suebin/stack & queue/같은숫자는싫어.py
+++ suebin/stack & queue/같은숫자는싫어.py
@@ -3,7 +3,7 @@ def solution(arr):
     answer = []
     
     for i in arr:
-        if not answer or answer[-1] != i: # 리스트 answer이 비었거나, 리스트 answer의 마지막 요소(answer[-1])가 리스트 arr의 요소(i)와 다른 경우 
-            answer.append(i) # 리스트 answer에 i를 추가한다.
+        if not answer or answer[-1] != i:  
+            answer.append(i) 
                 
     return answer
