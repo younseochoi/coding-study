64faa62315358708c30b48eff5e6b0f166790792
suebin <suuebin@gmail.com>
2022-09-29 23:51:20 +0900

    [스택/큐] 기능개발

---
+++ suebin/stack & queue/기능개발.py
@@ -0,0 +1,17 @@
+import math
+
+def solution(progresses, speeds):
+    
+    # 각 기능개발이 배포까지 걸리는 일 수
+    days = [math.ceil((100 - a) / b) for a, b in zip(progresses, speeds)]          
+    answer = []
+    index = 0
+    
+    for i in range(len(days)):
+        if days[index] < days[i]:
+            answer.append(i-index)
+            index = i
+        
+    answer.append(len(days) - index)
+    
+    return answer
