b205026b416e94b59114f2bf5bcd467f9e3c45b9
khg <pkgprud0707@pukyong.ac.kr>
2022-11-23 23:26:03 +0900

    [월간코드챌린지] n^2배열자르기

--- heagyoung/monthcode/n^2배열자르기.java
+++ heagyoung/monthcode/n^2배열자르기.java
@@ -5,10 +5,10 @@ class Solution {
         int[] answer = new int [len+1];
         
         int a=-1, b=-1, how=-1, where=-1;
-        long nn=n;
+        long nn=n; //long이라서 자료형변환
         for(long i=left;i<right+1;i++){
             a=(int)(i/nn); b=(int)(i%nn);
-            how = a<b ? b+1 : a+1;
+            how = a<b ? b+1 : a+1; //위치에 따라서 넣어주는 값 달리
             where=(int)(i-left);
             //System.out.println(a+", "+b+", "+how);
             answer[where] = how;
