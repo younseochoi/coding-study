920a456e8f57ea6c239bf16ae752af76c45357d0
suebin <suuebin@gmail.com>
2022-12-26 18:34:31 +0900

    [재귀] 팩토리얼

---
+++ suebin/boj/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int sum = factorial(N);
+		
+		System.out.println(sum);
+	}
+	
+	public static int factorial(int N) {
+		if (N <= 1) return 1;
+		return N * factorial(N-1);
+	}	
+}
